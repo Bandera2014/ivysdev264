ba85e6d9b61a1b09989cf4f745fb5ad6624c857c
Bandera2014 <nicholas.bander@evsck12.com>
2021-09-12 16:35:02 -0500

    cant get info from UI

--- mod3/Android_Calculator/app/src/main/java/com/example/android_calculator/MainActivity.kt
+++ mod3/Android_Calculator/app/src/main/java/com/example/android_calculator/MainActivity.kt
@@ -11,31 +11,34 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        Log.d("please work","oncreate")
+
         var n1 = editNumber1.text.toString()
         var n2 = editNumber2.text.toString()
 
         plusBTN.setOnClickListener{
             var sumResults = n1 + n2
             resultView.text = sumResults
-            Log.d("please work",sumResults)
+            Log.d("please work",sumResults+" added together")
+            Log.d("please work",(n1+n2)+" added together")
         }
 
         minusBTN.setOnClickListener{
             var minusResults = n1 + n2
             resultView.text = minusResults
-            Log.d("please work",minusResults)
+            Log.d("please work",minusResults+" minus together")
         }
 
         multiplyBTN.setOnClickListener{
             var multiplyResults = n1 + n2
             resultView.text = multiplyResults
-            Log.d("please work",multiplyResults)
+            Log.d("please work",multiplyResults+" mult together")
         }
 
         divideBTN.setOnClickListener{
             var divideResults = n1 + n2
             resultView.text = divideResults
-            Log.d("please work",divideResults)
+            Log.d("please work",divideResults+" div together")
         }
     }
 }

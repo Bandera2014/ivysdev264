7803737e5f80c1b24fd3b25c3d468f496d9745cf
Bandera2014 <nicholas.bander@evsck12.com>
2021-11-29 22:04:28 -0600

    finished part4

--- final/WorkingVersion/4LogOut/app/src/main/java/com/example/lab10/Settings.kt
+++ final/WorkingVersion/4LogOut/app/src/main/java/com/example/lab10/Settings.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatDelegate
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_settings.*
 
@@ -19,6 +20,18 @@ class Settings : AppCompatActivity() {
         logoutBTN.setOnClickListener {
             FirebaseAuth.getInstance().signOut()
             startActivity(Intent(this,Login::class.java))
+        }
+
+//        https://www.geeksforgeeks.org/how-to-create-a-dark-mode-for-a-custom-android-app-in-kotlin/
+        modeSwitch.setOnCheckedChangeListener{ _, isChecked ->
+                if (modeSwitch.isChecked) {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                modeSwitch.text = "Disable dark mode"
+            }
+            else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                modeSwitch.text = "Enable dark mode"
+            }
         }
 
     }
